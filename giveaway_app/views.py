46608cb410026c8a7ab10e93fadf7e9c650bbf50
Piotr <piotr.dylnicki@gmail.com>
2024-04-17 00:12:48 +0300

    4

--- giveaway_app/views.py
+++ giveaway_app/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from giveaway_app.models import Institution, Donation
 
 
 class IndexView(View):
@@ -20,3 +21,11 @@ class AddDonationView(View):
     def get(self, request):
         return render(request, 'giveaway_app/form.html')
 
+    # Donation.objects.all().distinct('institution').count()
+
+class IndexView(View):
+    def get(self, request):
+        count_donations = Donation.objects.count()
+        count_institution = Institution.objects.count()
+        institutions = Institution.objects.all()
+        return render(request, 'giveaway_app/index.html', {'count_donations': count_donations, 'count_institution': count_institution, 'institutions': institutions})
--- mysite/urls.py
+++ mysite/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('add/', AddDonationView.as_view(), name='add'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
